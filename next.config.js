98662fed216ac2afdb8a1c6c6ef8f7471f833b07
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-07-23 15:12:30 +0630

    Initinal Commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: [
+        'media-exp2.licdn.com',
+        'lh3.googleusercontent.com'
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import axios from "axios"
+import type { NextPage } from "next"
+import NoResults from "../components/NoResults";
+import VideoCard from "../components/VideoCard";
+import { Video } from '../types';
+interface IProps {
+  videos: Video[];
+}
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Home: NextPage = ({ videos }: IProps) => {
+  console.log(videos)
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length
+        ? videos?.map((video: Video) => (
+          <VideoCard post={video} key={video._id} />
+        ))
+        : <NoResults text={`No Videos`} />}
+    </div>
+  )
+}
+
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`);
+
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  }
+
+  return {
+    props: { videos: response.data },
+  };
+};
+
+export default Home
